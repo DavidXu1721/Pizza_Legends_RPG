e4a7f6ee38c217d3cf56c60cae9a90410b13aae2
David Xu <xudavid456@gmail.com>
2025-08-11 10:28:46 +0100

    Added another spin animation

--- src/scripts/BattleModule/BattleAnimations.js
+++ src/scripts/BattleModule/BattleAnimations.js
@@ -2,10 +2,27 @@ import utils from "../utils";
 
 
 const BattleAnimations = {
-    async spin(event, onComplete) {
+    async spin1(event, onComplete) {
         const element = event.caster.pizzaElement;
         const animationClassName = event.caster.team === "player" ?
-            "battle-spin-right" : "battle-spin-left"; // because the player is always on the left side
+            "battle-spin1-right" : "battle-spin1-left"; // because the player is always on the left side
+
+        element.classList.add(animationClassName);
+
+        //Remove class when animation is fully complete
+        element.addEventListener("animationend", () => {
+            element.classList.remove(animationClassName);
+        }, {once: true});
+
+        //Continue battle cycle right arounf when the pizzas collide
+        await utils.wait(100);
+        onComplete();
+    },
+
+    async spin2(event, onComplete) {
+        const element = event.caster.pizzaElement;
+        const animationClassName = event.caster.team === "player" ?
+            "battle-spin2-right" : "battle-spin2-left"; // because the player is always on the left side
 
         element.classList.add(animationClassName);
 
