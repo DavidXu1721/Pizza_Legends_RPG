9ad5db02d120add1df7cd6133c26cd4b70583358
David Xu <xudavid456@gmail.com>
2025-08-18 22:35:48 +0100

    testing some overlay stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import './styles/test.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -8,5 +9,10 @@ createRoot(document.getElementById('root')).render(
     <h1>PIZZA LEGENDS RPG</h1>
     <App />
     <p>[INSERT DESCRIPTION HERE]</p>
+
+    <div className='test-pizza-container'>
+      <img className='pizza' src= './src/assets/characters/pizzas/s002.png' />
+      <div className='overlay' ></div>
+    </div>
   </StrictMode>
 )
