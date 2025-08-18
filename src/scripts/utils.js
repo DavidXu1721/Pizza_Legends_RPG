f635e74ed111b8f722c94f05869e53040f382e66
David Xu <xudavid456@gmail.com>
2025-08-18 22:36:22 +0100

    added some util functions, (probably won't get used)

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -145,6 +145,56 @@ const utils = {
         });
     },
 
+    getAbsoluteOffsetFromBody( el ){   
+        // finds the offset of el from the body or html element
+        var _x = 0;
+        var _y = 0;
+        while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) )
+        {
+            _x += el.offsetLeft - el.scrollLeft + el.clientLeft;
+            _y += el.offsetTop - el.scrollTop + el.clientTop;
+            el = el.offsetParent;
+        }
+        return { top: _y, left: _x };
+    },
+
+    getAbsoluteOffsetFromGivenElement( el, relativeEl ){   
+        // finds the offset of el from relativeEl
+        var _x = 0;
+        var _y = 0;
+        while( el && el != relativeEl && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) )
+        {
+            _x += el.offsetLeft - el.scrollLeft + el.clientLeft;
+            _y += el.offsetTop - el.scrollTop + el.clientTop;
+            el = el.offsetParent;
+        }
+        return { top: _y, left: _x };
+    },
+
+    getAbsoluteOffsetFromRelative( el ){   
+        // finds the offset of el from the first parent with position: relative
+        var _x = 0;
+        var _y = 0;
+
+        let valString;
+        while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) )
+        {
+            _x += el.offsetLeft - el.scrollLeft + el.clientLeft;
+            _y += el.offsetTop - el.scrollTop + el.clientTop;
+            el = el.offsetParent;
+            if (el != null)
+            {
+                if (getComputedStyle !== 'undefined')
+                    valString = getComputedStyle(el, null).getPropertyValue('position');
+                else
+                    valString = el.currentStyle['position'];
+                if (valString === "relative")
+                    el = null;
+            }
+        }
+        return { top: _y, left: _x };
+    },
+
     getRandomFromArray(array) {
         return array[Math.floor(Math.random() * array.length)]
     },
