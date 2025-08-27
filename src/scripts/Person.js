5a1e716f23eb2a87139c19bde8c55a9a7d80696a
David Xu <xudavid456@gmail.com>
2025-08-27 23:28:22 +0100

    Fixed some player-NPC movement discrepencies and fixed an edge case with parallel cutscene events

--- src/scripts/Person.js
+++ src/scripts/Person.js
@@ -5,12 +5,13 @@ class Person extends GameObject {
     constructor(config) {
         super(config);
         this.movingProgressRemaining = 0;
+        this.standingProgressRemaining = 0; //TODO: the way it is now haveing a timeout in the "stand" behaviour code makes it so that if we pause the game, the standing time still "runs" so I want to have a standingProgressRemaining property that updates with the game loop
         this.isStanding = false; // this is to indicate when an NPC is currently in a stand command currently not necessary as I have already dealt with events not overriding other stand events
         this.isTryingToMove = false; // niche case, but in situations where NPCs are being blocked, I'd want them to still play the walking animation
         this.isPlayerControlled = config.isPlayerControlled || false
 
         this.directionVelocities = { 
-            // NOTE: velocities may not be accurately reflected in game, since the player can't move faster than 16 pixels per frame and any overflow in the pixels travelled will be lost, (eg, velocity of 15 will mean that in 2 frames, the character travels 30 pixels but that gets cut down to 16, effective speed is 8)
+            // NOTE: velocities may not be accurately reflected in game, since the player can't move faster than 16 pixels per frame and any overflow in the pixels travelled will be lost, (eg, velocity of 15 will mean that in 2 frames, the character should travel 30 pixels but that gets cut down to 16, effective speed is 8)
             // Due to the grid based movement of this game, this is fine for now.
             "up"    : ["y", -1],
             "down"  : ["y", 1],
@@ -29,7 +30,7 @@ class Person extends GameObject {
         // }
 
         if (this.movingProgressRemaining > 0){
-            this.updatePosition();
+            // do  nothing, this is an artifact from previous bad code
         } else {
             // Case: We are keyboard ready and have an arrow pressed
             if (!state.map.isCutscenePlaying && state.arrow && this.isPlayerControlled) { 
@@ -43,6 +44,9 @@ class Person extends GameObject {
             }
             
         }
+
+        this.updatePosition(); // An incredibly pedantic pet peeve, but putting the update position here instead up in if else clause makes it so that the person will immediately start moving then they are able too, if we only called update position when this.movingProgressRemaining > 0then the player will be ever so slightly slower than NPCs
+
         // updating the animations needs to be done here and not in the 'else' block, since for NPCs the behaviours are executed in the super.update, and therefore will never be reached when the NPC is constantly walking 
         this.updateSpriteAnimations(state);
         
@@ -67,13 +71,16 @@ class Person extends GameObject {
                 if (state.map.detectObstruction(this.x, this.y, this.direction)){
                     // we only set isTryingToMove to true when behaviour.retry is true since if this was a player in a non-cutscene event, trying to walk in an obstruction will cause isTryingToMove to become true, and it will only be set to false on the next successful movement, NOT when you let go of the direction, this will cause the hero's walking animation to play in cutscenes
                     if (behaviour.retry) {
-                        // if the cutscene is playing, then don't continue trying to walk, otherwise, do
-                        if (!this.map.isCutscenePlaying) {
+                        // if the cutscene is playing, then don't continue trying to walk, otherwise, do. EXCEPT if the walk is actually a part of the cutscene (typically only relevant in parallel cutscene events)
+                        if (!this.map.isCutscenePlaying || behaviour.isCutscene) {
+                            console.log("KEEP TRYING!!!!");
+                            
                             this.isTryingToMove = true
                             setTimeout(() => {
                                 this.startBehaviour(state, {...behaviour, isRetry: true})
                             }, 10) // tbh I don't like this, as it detachs from the main game loop
                         } else {
+                            console.log("ABORT");
                             this.isTryingToMove = false
                             utils.emitEvent("PersonWalkingComplete", {
                                 targetId: this.id
@@ -87,8 +94,7 @@ class Person extends GameObject {
                 };
 
                 //Ready to walk!
-                this.isTryingToMove = false
-                state.map.moveWall(this.x, this.y, this.direction);
+                this.isTryingToMove = false;
                 this.movingProgressRemaining = utils.withGrid(1);
                 break;
             case 'stand':
@@ -121,7 +127,7 @@ class Person extends GameObject {
         
         this.movingProgressRemaining -= moveDist;
 
-        if (this.movingProgressRemaining === 0) {
+        if (this.movingProgressRemaining === 0 && moveDist > 0 /* We also need to check if the person actually moved */) {
             //We finished the walk!
             utils.emitEvent("PersonWalkingComplete", {
                 targetId: this.id
